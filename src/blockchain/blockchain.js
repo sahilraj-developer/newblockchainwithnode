d8a3743e77bf85b21075b94064aedc5e3573d758
sahil <sahil27raj@gmail.com>
2023-10-31 11:33:21 +0530

    add the changes after solving the small errors

--- src/blockchain/blockchain.js
+++ src/blockchain/blockchain.js
@@ -1,11 +1,11 @@
-const { calculateHash, mineBlock } = require('./block');
+const crypto = require('crypto');
 
 function createBlockchain(difficulty) {
   const chain = [createGenesisBlock()];
   return {
     chain,
     difficulty,
-    createGenesisBlock,
+    createBlock,
     getLatestBlock,
     addBlock,
     isChainValid,
@@ -13,11 +13,28 @@ function createBlockchain(difficulty) {
 }
 
 function createGenesisBlock() {
-  return createBlock(0, '0', new Date().toISOString(), 'Genesis Block', 4); // Here, we assume a fixed difficulty of 4.
+  return createBlock(0, '0', new Date().toISOString(), 'Genesis Block', 4);
+}
+
+function createBlock(index, previousHash, timestamp, data, difficulty) {
+  const block = {
+    index,
+    previousHash,
+    timestamp,
+    data,
+    difficulty,
+    nonce: 0,
+    hash: '',
+  };
+  block.hash = calculateHash(block);
+  return block;
 }
 
 function getLatestBlock(blockchain) {
-  return blockchain.chain[blockchain.chain.length - 1];
+  const val=blockchain.chain?.length;
+  console.log("val",val)
+  console.log("valtttt",typeof(val))
+  return blockchain.chain[val - 1];
 }
 
 function addBlock(blockchain, newBlock) {
@@ -38,4 +55,20 @@ function isChainValid(blockchain) {
   return true;
 }
 
-module.exports = createBlockchain;
+function calculateHash(block) {
+  const data = block.index + block.previousHash + block.timestamp + JSON.stringify(block.data) + block.difficulty + block.nonce;
+  return crypto.createHash('SHA256').update(data).digest('hex');
+}
+
+function mineBlock(block, difficulty) {
+  while (block.hash.substring(0, difficulty) !== '0'.repeat(difficulty)) {
+    block.nonce++;
+    block.hash = calculateHash(block);
+  }
+}
+
+module.exports = {
+  createBlockchain,
+  createBlock,
+  createGenesisBlock, // Export the createGenesisBlock function.
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const createBlockchain = require('./blockchain/blockchain');
-const { createBlock } = require('./blockchain\/block');
+const { createBlockchain, createGenesisBlock, createBlock } = require('./blockchain/blockchain');
 
-
-const difficulty = 4; 
+const difficulty = 4;
 const myBlockchain = createBlockchain(difficulty);
 
+myBlockchain.chain.push(createGenesisBlock()); // Create the Genesis block.
 
 myBlockchain.addBlock(createBlock(1, myBlockchain.getLatestBlock(myBlockchain), new Date().toISOString(), 'Block 1 Data'));
 myBlockchain.addBlock(createBlock(2, myBlockchain.getLatestBlock(myBlockchain), new Date().toISOString(), 'Block 2 Data'));
@@ -12,9 +11,7 @@ myBlockchain.addBlock(createBlock(2, myBlockchain.getLatestBlock(myBlockchain), 
 const isBlockchainValid = myBlockchain.isChainValid(myBlockchain);
 console.log('Is the blockchain valid? ' + isBlockchainValid);
 
-
 myBlockchain.chain[1].data = 'Modified Block 1 Data';
-
 
 const isTamperedBlockchainValid = myBlockchain.isChainValid(myBlockchain);
 console.log('Is the tampered blockchain valid? ' + isTamperedBlockchainValid);
